b72c9a5aeeb914d560eef1fa447ca0d3899cb60f
Prima_Hutapea <Prima_Hutapea@KitaDalamBahaya-TPE14>
2025-06-13 17:53:08 +0700

    first commit

---
+++ Test Suites/5 day wheater and currentair pollution.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>5 day wheater and currentair pollution</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e93590ef-05f7-4100-8132-1a84e9a8f09c</testSuiteGuid>
+   <testCaseLink>
+      <guid>b9329092-9e17-4776-be4a-d2b4bae157bc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/5 day weather forecast of Jakarta Selatan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>63216f5b-e73c-4a89-bb54-42b8c11d02ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/current air pollution of Jakarta Selatan</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
